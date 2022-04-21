858b57086c2c0e28f7a0043e37c90770087bf914
jjunhouse <91825771+jjunhouse@users.noreply.github.com>
2022-04-21 18:06:24 +0900

    Add files via upload

--- array, linked list/study_hard_data_structure--master/array, linked list/doublylist/displayDoublyList.c
+++ array, linked list/study_hard_data_structure--master/array, linked list/doublylist/displayDoublyList.c
@@ -4,19 +4,20 @@
 void displayDoublyList(DoublyList* pList)
 {
 	DoublyListNode* currNode;
-	int				Length;
+	int				length;
 	int				i;
 	
 	if (pList == NULL)
-		printf("현재 노드는 비어있습니다?");
+		printf("현재 노드는 비어있습니다!!");
 	else
 	{
 		currNode = pList;
-		Length = getDoublyListLength(pList);
-		printf("총 노드의 수는 %d개 입니다.", Length);
-		while (!pList)
+		length = getDoublyListLength(pList);
+		printf("총 노드의 수는 %d개 입니다.\n", length);
+		i = 0;
+		while (currNode != NULL)
 		{
-			printf("node %d번째 요소는 : %d개 입니다.", i, currNode->currentElementCount);
+			printf("node %d번째 요소는 : %d개 입니다.\n", i, currNode->data);
 			i++;
 			currNode = currNode->pRLink;
 		}
